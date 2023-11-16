5365f23c811630fa55c9b7f5a8da0e92a841b374
Leandro Climaco <climacobnu@gmail.com>
2023-11-16 16:36:32 -0300

    .

---
+++ TrilhaDEV-Semana3/prova3/script2.js
@@ -0,0 +1,30 @@
+let i = 0;
+
+let sum = 0;
+
+
+
+while (i < 5) {
+
+  sum += i;
+
+  i++;
+
+}
+
+
+
+do {
+
+  sum--;
+  i--;
+
+} while (i > 0);
+
+
+
+for (let j = 0; j < 5; j++) {
+
+  sum *= 2;
+
+}
